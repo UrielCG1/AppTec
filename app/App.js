c21725248ecaea19f6015a07b5ca2cf0fd798170
Miguel Angel Ibarra <l20140189@queretaro.tecnm.mx>
2024-04-20 18:23:55 -0600

    Co-authored-by: Uriel Carbajal G. <UrielCG1@users.noreply.github.com> Co-authored-by: OscarEscamillaCast <OscarEscamillaCast@users.noreply.github.com>

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import LoginScreen from './pages/login'; // Importa LoginScreen desde './pages/login'
+
+const App = () => {
+  return (
+    <View>
+      <LoginScreen />
+    </View>
+  );
+};
+
+export default App;
